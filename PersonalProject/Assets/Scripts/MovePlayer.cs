d02718891cbec12e0b0074d310a01999b1ef7af4
Jeremiah00 <jeremiah.w.wade567@gmail.com>
2023-03-24 11:02:45 -0700

    Progress
    Finished the camera movement, and some of the player movement.

--- PersonalProject/Assets/Scripts/MovePlayer.cs
+++ PersonalProject/Assets/Scripts/MovePlayer.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class MovePlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+    float horziontalInput;
+    float verticalInput;
+
+    Rigidbody rb;
+    public Transform orientation;
+    public float moveSpeed;
+
+    Vector3 moveDirection;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horziontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
     }
+    void FixedUpdate()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horziontalInput;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f);
+    }
+
+    
+
+
 }
